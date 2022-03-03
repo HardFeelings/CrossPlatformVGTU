189993a6f8ee387dad251271829d8188198a2aff
HardFeelings <shatsckij.i@yandex.ru>
2022-03-03 19:12:05 +0300

    Добавление UserController

---
+++ src/ru/shatsckij/mainFrame/UserController.java
@@ -0,0 +1,12 @@
+package ru.shatsckij.mainFrame;
+
+import javax.swing.*;
+
+public class UserController {
+    public static void main(String[] args) {
+        JFrame mainFrame = new JFrame();
+        mainFrame.setVisible(true);
+        mainFrame.setBounds(0,0,0,0);
+        System.out.println();
+    }
+}
